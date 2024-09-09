e54fa045f7b1656defec163ee61671e933cf5270
Donald Murillo <donald.murillo07@gmail.com>
2024-09-08 23:43:15 -0400

    Added forgot password

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Account from './Account';
+import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -15,6 +16,7 @@ export function App() {
 					<Route path="/sign-in" element={<SignIn />} />
 					<Route path="/sign-up" element={<SignUp />} />
 					<Route path="/account" element={<Account />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
 				</Routes>
 			</div>
 		</Router>
