dc5b9b9bff19b30eaf0ef6bb24df0dc9de1dded5
Donald Murillo <donald.murillo07@gmail.com>
2024-09-08 22:34:58 -0400

    added react router and added basic routes

---
+++ src/app/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignIn: React.FC = () => {
+	return (
+		<div>
+			<h1>Sign In</h1>
+			{/* Add sign in form here */}
+		</div>
+	);
+};
+
+export default SignIn;
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,40 @@
-import NxWelcome from './nx-welcome';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Account from './Account';
+import Home from './Home';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
 export function App() {
 	return (
-		<div>
-			<NxWelcome title="basic-starter" />
-		</div>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/sign-in">Sign In</Link>
+						</li>
+						<li>
+							<Link to="/sign-up">Sign Up</Link>
+						</li>
+						<li>
+							<Link to="/account">Account</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/account" element={<Account />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
